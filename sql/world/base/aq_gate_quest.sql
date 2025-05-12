6de0c24662e49ca279015016e335de6486dd92b0
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-05-12 02:38:39 +0200

    aq gate sql adjustments

--- sql/world/base/aq_gate_quest.sql
+++ sql/world/base/aq_gate_quest.sql
@@ -45,10 +45,10 @@ UPDATE `creature_loot_template` SET `Chance` = 5 WHERE `Item` = 21146;
 UPDATE `creature_loot_template` SET `Chance` = 5 WHERE `Item` = 21147;
 UPDATE `creature_loot_template` SET `Chance` = 5 WHERE `Item` = 21148;
 
--- FIx drop rate for Narain's Scrying Goggles
+-- Fix drop rate for Narain's Scrying Goggles
 UPDATE `creature_loot_template` SET `Chance` = 2 WHERE `Item` = 20951;
 
--- New 'Bang a Gong!' quest. No rewards for completing this version. No mount, no title. Currently the Mallet of Zul'Farrak is required to bang the gong.
+-- New 'Bang a Gong! (Skip)' quest. No rewards for completing this version. No mount, no title.
 DELETE FROM `quest_template` WHERE `ID` = 108743;
 INSERT INTO `quest_template` (`ID`, `QuestType`, `QuestLevel`, `MinLevel`, `QuestSortID`, `QuestInfoID`, `SuggestedGroupNum`, `RequiredFactionId1`, `RequiredFactionId2`, `RequiredFactionValue1`, `RequiredFactionValue2`, `RewardNextQuest`, `RewardXPDifficulty`, `RewardMoney`, `RewardMoneyDifficulty`, `RewardDisplaySpell`, `RewardSpell`, `RewardHonor`, `RewardKillHonor`, 
 `StartItem`, `Flags`, `RequiredPlayerKills`, `RewardItem1`, `RewardAmount1`, `RewardItem2`, `RewardAmount2`, `RewardItem3`, `RewardAmount3`, `RewardItem4`, `RewardAmount4`, `ItemDrop1`, `ItemDropQuantity1`, `ItemDrop2`, `ItemDropQuantity2`, `ItemDrop3`, `ItemDropQuantity3`, `ItemDrop4`, `ItemDropQuantity4`, 
@@ -58,7 +58,6 @@ INSERT INTO `quest_template` (`ID`, `QuestType`, `QuestLevel`, `MinLevel`, `Ques
 `RequiredItemCount1`, `RequiredItemCount2`, `RequiredItemCount3`, `RequiredItemCount4`, `RequiredItemCount5`, `RequiredItemCount6`, `Unknown0`, `ObjectiveText1`, `ObjectiveText2`, `ObjectiveText3`, `ObjectiveText4`, `VerifiedBuild`) VALUES 
 (108743, 0, 60, 60, 1377, 82, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Bang a Gong! (Skip)', '', '', NULL, 'Return to The Scarab Gong in Silithus.', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3, '', '', '', '', 12340);
 
--- update quest texts for the new 'Bang a Gong!' quest
 DELETE FROM `quest_request_items` WHERE `ID` = 108743;
 INSERT INTO `quest_request_items` (`ID`, `EmoteOnComplete`, `EmoteOnIncomplete`, `CompletionText`, `VerifiedBuild`) VALUES 
 (108743, 1, 0, 'The Scarab Gong looms ominously before you. Steel yourself, $N; for once the Scarab Gong is rung, the gates of Ahn\'Qiraj will be opened.$B$BFrom the slackened maw of the beast can only come chaos and destruction. Defend your people!', 12340);
@@ -72,9 +71,9 @@ INSERT INTO `quest_template_addon` (`ID`, `MaxLevel`, `AllowableClasses`, `Sourc
 `RequiredSkillID`, `RequiredSkillPoints`, `RequiredMinRepFaction`, `RequiredMaxRepFaction`, `RequiredMinRepValue`, `RequiredMaxRepValue`, `ProvidedItemCount`, `SpecialFlags`) VALUES 
 (108743, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     
--- Connect new 'Bang a Gong!' quest to the Scarab Gong in Silithus
--- DELETE FROM `gameobject_queststarter` WHERE `quest` = 108743;
+-- Connect new 'Bang a Gong! (Skip)' quest to the Scarab Gong in Silithus
+DELETE FROM `gameobject_queststarter` WHERE `quest` = 108743;
 -- INSERT INTO `gameobject_queststarter` (`id`, `quest`) VALUES (180717, 108743);
 
--- DELETE FROM `gameobject_questender` WHERE `quest` = 108743;;
+DELETE FROM `gameobject_questender` WHERE `quest` = 108743;;
 -- INSERT INTO `gameobject_questender` (`id`, `quest`) VALUES (180717, 108743);
