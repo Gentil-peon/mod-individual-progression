3c50b10d04a8ed15958be7c1bebc6f43af541721
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-05-28 02:57:34 +0200

    phasing icc phase

--- sql/world/base/zz_optional_wotlk_phasing.sql
+++ sql/world/base/zz_optional_wotlk_phasing.sql
@@ -11,7 +11,8 @@ WHERE `map` = 571 AND `id1` IN (
 36657, -- Sunreaver War Mage, Icecrown
 37742, -- Drugan Deepdraught, Icecrown
 37965, -- Argent Commander, Icecrown
-37967 -- Ebon Blade Commander, Icecrown
+37967, -- Ebon Blade Commander, Icecrown
+37968 -- Argent Hippogryph, Icecrown
 );
 
 UPDATE `gameobject` SET `ScriptName` = 'gobject_ipp_wotlk_icc' 
