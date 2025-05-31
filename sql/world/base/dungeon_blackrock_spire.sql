5739d811b023432f26f96ce34d9802057f36425d
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-06-01 01:06:20 +0200

    fix overflow

--- sql/world/base/dungeon_blackrock_spire.sql
+++ sql/world/base/dungeon_blackrock_spire.sql
@@ -30,4 +30,4 @@ UPDATE `creature_loot_template` SET `Chance` = 33 WHERE `Item` IN (12335, 12336,
 /* Npc for seal of ascension quest */
 DELETE FROM `creature` WHERE `id1` = 10296;
 INSERT INTO `creature` (`guid`,`id1`,`id2`,`id3`,`map`,`zoneId`,`areaId`,`spawnMask`,`phaseMask`,`equipment_id`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`wander_distance`,`currentwaypoint`,`curhealth`,`curmana`,`MovementType`,`npcflag`,`unit_flags`,`dynamicflags`,`ScriptName`,`VerifiedBuild`,`CreateObject`,`Comment`) VALUES
-(90010296, 10296, 0, 0, 229, 0, 0, 1, 1, 0, 45.7918, -334.239, 53.8074, 2.19976, 300, 0, 0, 43705, 0, 0, 0, 0, 0, '', 0, 0, '');
+(9010296, 10296, 0, 0, 229, 0, 0, 1, 1, 0, 45.7918, -334.239, 53.8074, 2.19976, 300, 0, 0, 43705, 0, 0, 0, 0, 0, '', 0, 0, '');
