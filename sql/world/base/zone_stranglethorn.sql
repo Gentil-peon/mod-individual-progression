1b94ffa77d0be441b0713da1ad8eace821c2d189
valsan-azerty-boi <52854501+valsan-azerty-boi@users.noreply.github.com>
2025-06-14 23:38:51 +0200

    change some phasing

--- sql/world/base/zone_stranglethorn.sql
+++ sql/world/base/zone_stranglethorn.sql
@@ -6,3 +6,9 @@ UPDATE `creature_loot_template` SET `Chance` = 4 WHERE `Item` = 3862;
 
 /* Drop chance for Jungle Stalker Feather was incorrectly set to 80 - updated to 25 */
 UPDATE `creature_loot_template` SET `Chance` = 25 WHERE `Item` = 3863;
+
+/* Landro for TCG promotion in Booty Bay */
+UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_aq' WHERE `entry` = 17249;
+
+/* Landro Hired Bodyguard in Booty Bay */
+UPDATE `creature_template` SET `ScriptName` = 'npc_ipp_tbc' WHERE `entry` = 21045;
